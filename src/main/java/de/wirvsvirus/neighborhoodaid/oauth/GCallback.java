6fa0c179c69009bfe0765d693bd9d9f50c7a49cc
BuildTools <unconfigured@null.spigotmc.org>
2020-03-21 16:23:35 +0100

    GAuth versuch 2

---
+++ src/main/java/de/wirvsvirus/neighborhoodaid/oauth/GCallback.java
@@ -0,0 +1,14 @@
+package de.wirvsvirus.neighborhoodaid.oauth;
+
+import de.wirvsvirus.neighborhoodaid.api.Endpoint;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import org.jetbrains.annotations.NotNull;
+
+public class GCallback implements Endpoint {
+
+    @Override
+    public void setupRouting(@NotNull Vertx vertx, @NotNull Router router) {
+        router.r
+    }
+}
