c88524b8da5de45acd9e76e0b3a34b89488a9e20
Dudeplayz <Dario@ok.de>
2020-03-22 15:13:41 +0100

    Add basic User endpoints (#12)
    * add: UserEndpoint for update and get
    
    * merge: User, RestVerticle; increment bugfix version

---
+++ src/main/java/de/wirvsvirus/neighborhoodaid/api/user/UserEndpoint.java
@@ -0,0 +1,82 @@
+package de.wirvsvirus.neighborhoodaid.api.user;
+
+import de.wirvsvirus.neighborhoodaid.api.Endpoint;
+import de.wirvsvirus.neighborhoodaid.api.utils.RestUtils;
+import de.wirvsvirus.neighborhoodaid.db.UserDAO;
+import de.wirvsvirus.neighborhoodaid.db.model.User;
+import de.wirvsvirus.neighborhoodaid.geolocation.GeoLocation;
+import de.wirvsvirus.neighborhoodaid.utils.DbUtils;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class UserEndpoint implements Endpoint {
+    @Override
+    public void setupRouting(@NotNull Vertx vertx, @NotNull Router router) {
+        router.get("/:id").handler(ctx -> getUser(vertx, ctx));
+        router.post("/:id").handler(ctx -> updateUser(vertx, ctx));
+    }
+
+    private void getUser(Vertx vertx, RoutingContext ctx) {
+        final var id = ctx.request().getParam("id");
+        try {
+            final var uuid = UUID.fromString(id);
+            DbUtils.getDbAccessor(vertx, accessor -> {
+                final var user = accessor.getRoot().getUsers().get(uuid);
+                if (user == null) {
+                    RestUtils.endResponseWithError(ctx, 404, "User with id '" + uuid + "' not found.");
+                } else {
+                    ctx.response().end(Json.encodePrettily(user));
+                }
+            });
+        } catch (IllegalArgumentException ex) {
+            RestUtils.endResponseWithError(ctx, 400, ex.getLocalizedMessage());
+        }
+    }
+
+    private void updateUser(Vertx vertx, RoutingContext ctx) {
+        final var id = ctx.request().getParam("id");
+        try {
+            final var uuid = UUID.fromString(id);
+            DbUtils.getDbAccessor(vertx, accessor -> {
+                final var user = accessor.getRoot().getUsers().get(uuid);
+                if (user == null) {
+                    RestUtils.endResponseWithError(ctx, 404, "User with id '" + uuid + "' not found.");
+                } else {
+                    final var body = getUserFromBody(ctx);
+                    final var address = body.getAddress();
+                    if (address == null) {
+                        RestUtils.endResponseWithError(ctx, 500, "Missing address field!");
+                    }
+                    else{
+                    GeoLocation.requestGeoLocation(vertx, address, res -> {
+                        final var dao = new UserDAO(accessor);
+                        if (res.succeeded()) {
+                            final var newUser = dao.updateUser(uuid, body.withNewAddress(res.result()));
+                            ctx.response().end(Json.encodePrettily(newUser));
+                        } else {
+                            RestUtils.endResponseWithError(ctx, 500, "Could not request address: " + res.cause().getLocalizedMessage());
+                        }
+                    });
+                    }
+                }
+            });
+        } catch (IllegalArgumentException ex) {
+            RestUtils.endResponseWithError(ctx, 400, ex.getLocalizedMessage());
+        }
+    }
+
+    private User getUserFromBody(RoutingContext ctx) {
+        try {
+            final var json = ctx.getBodyAsJson();
+            return json.mapTo(User.class);
+        } catch (Throwable ex) {
+            RestUtils.endResponseWithError(ctx, 400, "Json exception: " + ex.getLocalizedMessage());
+        }
+        return null;
+    }
+}
